27e8d0ec9803397e51d74a4a4fdb20251c1f0bd9
Deepanshugupta65 <dj911743@gmail.com>
2026-03-19 11:46:59 +0530

    authentication and authorization

--- app/api/task.py
+++ app/api/task.py
@@ -8,6 +8,8 @@ from app.schema.task import TaskCreate
 from app.schema.task import TaskResponse,TaskUpdate
 from typing import List
 
+# get_current_user protected routes
+from app.core.security import get_current_user
 
 #  api router : used to group related routes
 # depends fastapi depedncy injection system
@@ -29,11 +31,13 @@ def get_db():
 
 # before exeute the fun run task:create it will check the validation and db: session is for 
 @router.post("/", status_code=201)
-def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+def create_task(task: TaskCreate, db: Session = Depends(get_db),current_user = Depends(get_current_user)):
     try:    
         # concept : swlachemy is orm it maps databse table  < --->  python class .... so in code tasks table <--> Task class
         # 1. Create python object
-        new_task = Task(title=task.title)
+
+        # new_task = Task(title=task.title)
+        new_task = Task(title=task.title,owner_id=current_user.id)
         
         # 2. Add to session
         db.add(new_task)
@@ -55,21 +59,29 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
 # get all task
 # if response_model removed it convert swlachemy object to json .. return all field from the model, no validation of output ..no constrol over exposed data 
 @router.get("/", response_model=List[TaskResponse])
-def get_all_tasks(db: Session = Depends(get_db)):
-    tasks = db.query(Task).all()
+def get_all_tasks(db: Session = Depends(get_db),current_user = Depends(get_current_user)):
+
+    if current_user.role == "admin":
+        tasks = db.query(Task).all()
+    else:
+        tasks = db.query(Task).filter(Task.owner_id == current_user.id).all()    
     return tasks
 
 
 # delete task
 
 @router.delete("/{task_id}")
-def delete_task(task_id:int,db:Session=Depends(get_db)):
+def delete_task(task_id:int,db:Session=Depends(get_db),current_user = Depends(get_current_user)):
 
     # find task
     task=db.query(Task).filter(Task.id==task_id).first()
     # if not found
     if not task:
         raise HTTPException(status_code=404,detail="Task not found")
+    # get_current_user
+    # check ownership or admin 
+    if task.owner_id != current_user.id and current_user.role != "admin":
+        raise HTTPException(status_code=403,detail="Not authorized")
     # delter task
     db.delete(task)
     db.commit()
@@ -77,15 +89,17 @@ def delete_task(task_id:int,db:Session=Depends(get_db)):
     return {"message":"Task deleted successfully"}
 
 # uodate the task
-@router.put("/tasks/{task_id}", response_model=TaskResponse)
-def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db)):
+@router.put("/{task_id}", response_model=TaskResponse)
+def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db),current_user = Depends(get_current_user)):
 
     # Find task
     db_task = db.query(Task).filter(Task.id == task_id).first()
 
     if not db_task:
         raise HTTPException(status_code=404, detail="Task not found")
-
+    # check ownership or admin
+    if db_task.owner_id != current_user.id and current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Not authorized")
     # Update fields if provided
     if task.title is not None:
         db_task.title = task.title
--- app/core/security.py
+++ app/core/security.py
@@ -8,6 +8,8 @@ pwd_context = CryptContext(
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+# login api 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
@@ -21,3 +23,60 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(hours=2)
     data.update({"exp": expire})
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+
+#  get_current_user()
+
+# depend used for dependcy injec , httpex- used for error like 401,403
+from fastapi import Depends, HTTPException
+# httpbeared -> read tokens from request header, httpauthcre- -> holds token info
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+# jwt used to decode token , jwterror -> handle invlaid token
+from jose import jwt, JWTError
+#  used for db operations
+from sqlalchemy.orm import Session
+
+# created db session , user ur table model
+from app.db.session import SessionLocal
+from app.model.user import User
+
+# this read authorization bearer token
+security = HTTPBearer()
+
+def get_db():
+    # create db connection
+    db = SessionLocal()
+    try:
+        # send db session to fun using it
+        yield db
+    finally:
+        # closes db after req ends
+        db.close()
+
+def get_current_user(
+        # read toekn from header and stores in credentials , db session injected
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+):
+    # extract actual token string
+    token = credentials.credentials
+
+    try:
+        # verify token and extract data inside token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # extract user_id from token
+        user_id = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
+        # if token is expired , modefied ,wrong then reject requests
+        raise HTTPException(status_code=401, detail="Invalid token")
+    #  find user in database
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user           
